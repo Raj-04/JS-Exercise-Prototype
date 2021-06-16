0331d4afac1a795cf2be0db4461936aad236656a
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-06-16 13:55:00 -0400

    completed task1

--- index.js
+++ index.js
@@ -39,16 +39,33 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length <= 10){
+       this.stomach.push(edible);
+    }
+  }
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
   }
  
- 
+  // const raj = new Person('Raj', 21);
+  // const romy = new Person('Romy', 29);
+  // const jordan = new Person('Jordan', 30);
 
-  
-  
-  
-  
+  // console.log(raj);
+  // console.log(romy);
+  // console.log(jordan);
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
